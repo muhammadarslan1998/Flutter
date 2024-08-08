2cbdb8ca28d9fa89a79fb2f6549b8296b7b4f43f
imac <mu44414@gmail.com>
2024-08-08 14:35:30 +0500

    new push

--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,58 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( debugShowCheckedModeBanner: false,
-      home: (Scaffold( backgroundColor: Colors.grey,
-        appBar: AppBar
-        ( backgroundColor: Colors.blueGrey,
-          
-          title:Center(child: (Text("Login"))) ,),
-        body: Center(
-          child: (Column
-          
-          (
-children: [  SizedBox(height: 60,),  Row(children: [ 
-  SizedBox(width: 40,), 
-  Text("Email"), 
-  SizedBox(width: 20, ),
-  Container(
-    width: 150,
-    child: TextField()),],),
- 
-           SizedBox(height: 40,),
-            Row(children: [ SizedBox(width: 40,),
-               Text("Password"),
-               
-               Container(
-                 width: 150,  child: TextField()),
-              
-             ],),
-           SizedBox(height: 30,), ElevatedButton(  style: ElevatedButton.styleFrom(
-    primary: Colors.blueGrey, // background
-    onPrimary: Colors.white, // foreground
-  ),  onPressed: (){}, child: Text("Login"),), ],)),
-        ),
-        
-      )));
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: (Scaffold(
+          backgroundColor: Colors.grey,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey,
+            title: Center(child: (Text("Logins"))),
+          ),
+          body: Center(
+            child: (Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text("Emails"),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(width: 150, child: TextField()),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text("Password"),
+                    Container(width: 150, child: TextField()),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueGrey, // background
+                    onPrimary: Colors.white, // foreground
+                  ),
+                  onPressed: () {},
+                  child: Text("Login"),
+                ),
+              ],
+            )),
+          ),
+        )));
   }
 }
